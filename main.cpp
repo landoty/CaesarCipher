e265e424650b4843ecea364f7676d612f831f10a
Landen Doty <landot2002@gmail.com>
2020-12-21 11:36:12 -0600

    Updated variable names and error messages
    Corrects encryption semantics for easier reading
    
    eText->cipherTxt 
    dText->decipherTxt
    cipherText->plainText 
    
    <ciphertext> -> <plaintext>

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 
-std::string encrypt(std::string eText)
+std::string encrypt(std::string cipherTxt)
 {
     char currentLetter;
     int key;
@@ -9,14 +9,14 @@ std::string encrypt(std::string eText)
     std::cout << "Enter an encryption key: ";
     std::cin >> key;
 
-    for(int i = 0; i < eText.length(); ++i)
+    for(int i = 0; i < cipherTxt.length(); ++i)
     {
-        currentLetter = eText[i];
+        currentLetter = cipherTxt[i];
         //if letter is lowercase
         if(currentLetter >= 'a' && currentLetter <= 'z')
         {
             currentLetter += key;
-            eText[i] = currentLetter;
+            cipherTxt[i] = currentLetter;
             //bound by letters only. Will go to start of alphabet if goes past 'z'
             if(currentLetter > 'z')
             {
@@ -32,12 +32,12 @@ std::string encrypt(std::string eText)
             {
                 currentLetter = currentLetter - 'Z' + 'A' - 1;
             }
-            eText[i] = currentLetter;
+            cipherTxt[i] = currentLetter;
         }
     }
-    return(eText);
+    return(cipherTxt);
 }
-std::string decrypt(std::string dText)
+std::string decrypt(std::string decipherTxt)
 {
     char currentLetter;
     int key;
@@ -46,9 +46,9 @@ std::string decrypt(std::string dText)
     std::cin >> key;
 
     //algorithm is the same as encryption, but subtracts key instead of adding it
-    for(int i = 0; i < dText.length(); ++i)
+    for(int i = 0; i < decipherTxt.length(); ++i)
     {
-        currentLetter = dText[i];
+        currentLetter = decipherTxt[i];
         if(currentLetter >= 'a' && currentLetter <= 'z')
         {
             currentLetter -= key;
@@ -66,34 +66,34 @@ std::string decrypt(std::string dText)
             {
                 currentLetter = currentLetter + 'Z' - 'A' + 1;
             }
-            dText[i] = currentLetter;
+            decipherTxt[i] = currentLetter;
         }
     }
-    return(dText);
+    return(decipherTxt);
 }
 int main(int argc, char** argv)
 {
     if(argc != 3)
     {
-        std::cout << "Invalid input. Enter as \"./ceaser <ciphertext> <e for encrypt or d decrypt>\"\n";
+        std::cout << "Invalid input. Enter as \"./ceaser <plaintext> <e for encrypt or d decrypt>\"\n";
     }
     //assignging to strings makes the proceeding logic function correctly
     else
     {
-        std::string cipherText = argv[1];
+        std::string plainText = argv[1];
         std::string type = argv[2];
         if(type == "encrypt" || type == "Encrypt")
         {
-            std::cout << encrypt(cipherText) << "\n";
+            std::cout << encrypt(plainText) << "\n";
         }
         else if(type == "decrypt" || type == "Decrypt")
         {
-            std::cout << decrypt(cipherText) << "\n";
+            std::cout << decrypt(plainText) << "\n";
         }
         //in case user doesn't enter 'encrypt' or 'decrypt' or text contains spacees
         else
         {
-        std::cout << "Invalid input. Enter as \"./ceaser <ciphertext> <e for encrypt or d decrypt>\"\nUse quotations if cipher text contains spaces\n";
+        std::cout << "Invalid input. Enter as \"./ceaser <plaintext> <e for encrypt or d decrypt>\"\nUse quotations if cipher text contains spaces\n";
         }
     }
     return 0;
